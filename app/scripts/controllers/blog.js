7d27c293005feaa743f7fca4fb963442cc2e7363
PiotrKulpa <p.kulpa@onet.eu>
2016-11-19 17:23:13 +0100

    footer

--- app/scripts/controllers/blog.js
+++ app/scripts/controllers/blog.js
@@ -11,8 +11,20 @@ angular.module('landscapesApp')
   .controller('BlogCtrl', function ($scope, $http) {
     $http.get('/scripts/data.json').then(function(response){
       $scope.news = response.data.records;
+      var myDataCount = $scope.news.length;
+       var paginationIndex = Math.ceil(myDataCount / 10);
+
+       $scope.paginationData = [];
+       var i =0;
+       for(i=0; i<paginationIndex; i++){
+         $scope.paginationData.push(i);
+       };
     }, function(response){
       $scope.err = 'Sorry, cant load data';
     });
+      $scope.n = 0;
+      $scope.paginationRange = function(pageNumber) {
+      $scope.n = pageNumber * 10;
+      };
 
   });
